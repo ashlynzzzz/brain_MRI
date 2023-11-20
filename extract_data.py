168199d9e71a1aae5d04e5561f7fa3a1ad0358f1
Yinhan-Chen <chen2855@purdue.edu>
2023-11-20 17:40:31 -0500

    2

--- extract_data.py
+++ extract_data.py
@@ -10,11 +10,13 @@ def extract_data(fp):
     object1: shoe
     9 pictures for each run each object
     '''
-    z_value = 31
-    x_train = np.zeros((40, 64, 198)) # (x, y, number of pictures)  198 = 2*9*11
+    pic_num = 9  # number of pictures for each object each run
+    offset = 1 # shift for response delay
+    z_value = [27, 29, 31, 33, 35]
+    x_train = np.zeros((40, 64, 5, 198)) # (x, y, z, number of pictures)  198 = 2*9*11
     y_train = np.zeros(198)
-    x_test = np.zeros((40, 64, 18))
-    y_test = np.zeros(18)
+    x_test = np.zeros((40, 64, 5, 2*pic_num))
+    y_test = np.zeros(2*pic_num)
     # object location for 12 runs
     filter = np.array([[5, 0, 48, 0],   # 12, 120
                         [63, 0, 34, 0],  # 156, 84
@@ -43,13 +45,19 @@ def extract_data(fp):
         image = nib.load(file_path)
         data = image.get_fdata()
         
+        # Get object start number
         obj0s, obj0e, obj1s, obj1e = filter[x-1, :]
-        
-        x_train[:, :, 18*(x-1):18*(x-1)+9] = data[:, :, z_value, obj0s:obj0s+9]
-        y_train[18*(x-1):18*(x-1)+9] = 0
-        x_train[:, :, 18*(x-1)+9:18*(x-1)+18] = data[:, :, z_value, obj1s:obj1s+9]
-        y_train[18*(x-1)+9:18*(x-1)+18] = 1
-        
+        obj0s += offset
+        obj1s += offset
+        i = 0
+
+        for z in z_value:
+            x_train[:, :, i, 2*pic_num*(x-1) : 2*pic_num*(x-1)+pic_num] = data[:, :, z, obj0s : obj0s+pic_num]
+            y_train[2*pic_num*(x-1) : 2*pic_num*(x-1)+pic_num] = 0
+            x_train[:, :, i, 2*pic_num*(x-1)+pic_num : 2*pic_num*(x-1)+2*pic_num] = data[:, :, z, obj1s : obj1s+pic_num]
+            y_train[2*pic_num*(x-1)+pic_num : 2*pic_num*(x-1)+2*pic_num] = 1
+            i += 1
+
     # Create testing data
     file_path = fp + '/sub-1/func/sub-1_task-objectviewing_run-12_bold.nii.gz'
 
@@ -57,15 +65,20 @@ def extract_data(fp):
     data = image.get_fdata()
 
     obj0s, obj0e, obj1s, obj1e = filter[11, :]
-
-    x_test[:, :, 0:9] = data[:, :, z_value, obj0s:obj0s+9]
-    y_test[0:9] = 0
-    x_test[:, :, 9:18] = data[:, :, z_value, obj1s:obj1s+9]
-    y_test[9:18] = 1
+    obj0s += offset
+    obj1s += offset
+    i = 0
+    
+    for z in z_value:
+        x_test[:, :, i, 0:pic_num] = data[:, :, z, obj0s:obj0s+pic_num]
+        y_test[0:pic_num] = 0
+        x_test[:, :, i, pic_num:2*pic_num] = data[:, :, z, obj1s:obj1s+pic_num]
+        y_test[pic_num:2*pic_num] = 1
+        i += 1
 
     return x_train, y_train, x_test, y_test
 
 
-x_train, y_train, x_test, y_test = extract_data('project/ds000105_R2.0.2_raw')
+x_train, y_train, x_test, y_test = extract_data('')
 print(y_train)
 print(np.count_nonzero(x_train == 0))
