f220ba19da8c02d3eaaa149a37c59f64e6c5d920
lznasta <chen2855@purdue.edu>
2023-11-26 21:13:44 -0500

    1

--- extract_data.py
+++ extract_data.py
@@ -11,12 +11,14 @@ def extract_data(fp):
     9 pictures for each run each object
     '''
     pic_num = 9  # number of pictures for each object each run
-    offset = 1 # shift for response delay
+    offset = 2 # shift for response delay
     z_value = [27, 29, 31, 33, 35]
-    x_train = np.zeros((40, 64, 5, 198)) # (x, y, z, number of pictures)  198 = 2*9*11
-    y_train = np.zeros(198)
-    x_test = np.zeros((40, 64, 5, 2*pic_num))
-    y_test = np.zeros(2*pic_num)
+    x_data = np.zeros((40, 64, 5, 2*2*9*12)) # (x, y, z, number of pictures)  432 = 2*2*9*12
+    y_data = np.zeros(2*2*9*12)
+    x_train = np.zeros((40, 64, 5, 2*2*9*11))
+    y_train = np.zeros(2*2*9*11)
+    x_test = np.zeros((40, 64, 5, 2*2*9*1))
+    y_test = np.zeros(2*2*9*1)
     # object location for 12 runs
     filter = np.array([[5, 0, 48, 0],   # 12, 120
                         [63, 0, 34, 0],  # 156, 84
@@ -29,56 +31,71 @@ def extract_data(fp):
                         [106, 0, 77, 0],   # 264, 192
                         [63, 0, 92, 0],   # 156, 228
                         [106, 0, 92, 0],   # 264, 156
+                        [106, 0, 77, 0], # 264, 192 
+                        # sub2
+                        [5, 0, 48, 0],   # 12, 120
+                        [34, 0, 48, 0],  # 84, 120
+                        [106, 0, 77, 0],   # 264, 192
+                        [106, 0, 5, 0],   # 264, 12
+                        [63, 0, 34, 0],   # 156, 84
+                        [34, 0, 77, 0],   # 84, 192
+                        [20, 0, 77, 0],   # 48, 192
+                        [34, 0, 48, 0],   # 84, 120
+                        [63, 0, 92, 0],   # 156, 228
+                        [106, 0, 48, 0],   # 264, 120
+                        [106, 0, 92, 0],   # 264, 156
                         [106, 0, 77, 0]])  # 264, 192 
 
     # Create training data
-    for x in range(1, 12):
-
-        if(x < 10):
-            y = "0" + str(x)
-        else:
-            y = str(x)
-        # File path
-        file_path = fp + f'/sub-1/func/sub-1_task-objectviewing_run-{y}_bold.nii.gz'
-
-        # Load the data
-        image = nib.load(file_path)
-        data = image.get_fdata()
+    # Go through all the sub
+    for s in range(1, 3):
         
-        # Get object start number
-        obj0s, obj0e, obj1s, obj1e = filter[x-1, :]
-        obj0s += offset
-        obj1s += offset
-        i = 0
+        # Go through all the run
+        for x in range(1, 13):
 
-        for z in z_value:
-            x_train[:, :, i, 2*pic_num*(x-1) : 2*pic_num*(x-1)+pic_num] = data[:, :, z, obj0s : obj0s+pic_num]
-            y_train[2*pic_num*(x-1) : 2*pic_num*(x-1)+pic_num] = 0
-            x_train[:, :, i, 2*pic_num*(x-1)+pic_num : 2*pic_num*(x-1)+2*pic_num] = data[:, :, z, obj1s : obj1s+pic_num]
-            y_train[2*pic_num*(x-1)+pic_num : 2*pic_num*(x-1)+2*pic_num] = 1
-            i += 1
+            if(x < 10):
+                y = "0" + str(x)
+            else:
+                y = str(x)
+            # File path
+            file_path = fp + f'/sub-{s}/func/sub-{s}_task-objectviewing_run-{y}_bold.nii.gz'
 
-    # Create testing data
-    file_path = fp + '/sub-1/func/sub-1_task-objectviewing_run-12_bold.nii.gz'
+            # Load the data
+            image = nib.load(file_path)
+            data = image.get_fdata()
+            
+            # Get object start number
+            obj0s, obj0e, obj1s, obj1e = filter[12*(s-1) + x-1, :]
+            obj0s += offset
+            obj1s += offset
+            i = 0
 
-    image = nib.load(file_path)
-    data = image.get_fdata()
+            # Go through all the slice
+            for z in z_value:
+                x_data[:, :, i, 216*(s-1) + 2*pic_num*(x-1) : 216*(s-1) + 2*pic_num*(x-1)+pic_num] = data[:, :, z, obj0s : obj0s+pic_num]
+                y_data[216*(s-1) + 2*pic_num*(x-1) : 216*(s-1) + 2*pic_num*(x-1)+pic_num] = 0
+                x_data[:, :, i, 216*(s-1) + 2*pic_num*(x-1)+pic_num : 216*(s-1) + 2*pic_num*(x-1)+2*pic_num] = data[:, :, z, obj1s : obj1s+pic_num]
+                y_data[216*(s-1) + 2*pic_num*(x-1)+pic_num : 216*(s-1) + 2*pic_num*(x-1)+2*pic_num] = 1
+                i += 1
 
-    obj0s, obj0e, obj1s, obj1e = filter[11, :]
-    obj0s += offset
-    obj1s += offset
-    i = 0
+    # Shuffle the data set
+    # Generate a permutation of indices
+    perm = np.random.permutation(2*2*9*12)
+    # Apply the permutation to both arrays
+    x_data = x_data[:, :, :, perm]
+    y_data = y_data[perm]
     
-    for z in z_value:
-        x_test[:, :, i, 0:pic_num] = data[:, :, z, obj0s:obj0s+pic_num]
-        y_test[0:pic_num] = 0
-        x_test[:, :, i, pic_num:2*pic_num] = data[:, :, z, obj1s:obj1s+pic_num]
-        y_test[pic_num:2*pic_num] = 1
-        i += 1
-
+    # take 11/12 as training set
+    x_train = x_data[:, :, :, 0: 397]
+    y_train = y_data[0: 397]
+    # take 1/12 as testing set
+    x_test = x_data[:, :, :, 397: 433]
+    y_test = y_data[397: 433]
+    
     return x_train, y_train, x_test, y_test
 
 
-# x_train, y_train, x_test, y_test = extract_data('')
-# print(y_train)
-# print(np.count_nonzero(x_train == 0))
+x_train, y_train, x_test, y_test = extract_data('project/ds000105_R2.0.2_raw')
+#print(y_train)
+print(np.count_nonzero(y_train))
+print(np.count_nonzero(y_test))
